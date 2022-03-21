817bb34dfb11fc21a4544f88edddcbde8f7375a4
Benjamin_Ajisefinni <benfinnirr@gmail.com>
2022-03-21 10:29:06 +0900

    Created Structure for week 2 Assignment

---
+++ week2/src/app.js
@@ -0,0 +1,17 @@
+var object ={
+    name: 'Bob',
+    age: 50
+};
+
+class Person{
+    name;
+    age;
+
+    constructor(name, age) {
+        this.name;
+        this.age;
+    }
+
+}// end class person
+
+console.log(new Person('Bob', 50));
